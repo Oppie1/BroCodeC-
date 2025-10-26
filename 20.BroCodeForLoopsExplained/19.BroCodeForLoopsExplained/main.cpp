9279b50a09df5e7dcac30e333642801cb7a03b64
Oppie1 <atindall729@gmail.com>
2025-10-26 13:27:45 -0500

    Add files via upload

---
+++ 20.BroCodeForLoopsExplained/19.BroCodeForLoopsExplained/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+
+{
+	for (int index = 1; index <= 3; index++) {
+		cout << "Happy New Year" << '\n';
+	}
+
+	cout << '\n';
+
+	for (int i = 1; i <= 3; i++) {
+		cout << "Happy New Year" << '\n';
+	}
+
+	cout << '\n';
+
+	for (int i = 1; i <= 5; i++) {
+		cout << "Happy New Year" << '\n';
+	}
+
+	cout << '\n';
+
+	for (int i = 1; i <= 10; i++) {
+		cout << i << '\n';
+	}
+	cout << "Happy New Year" << '\n';
+
+	cout << '\n';
+
+	for (int i = 1; i <= 10; i += 2) {
+		cout << i << '\n';
+	}
+	cout << "Happy New Year" << '\n';
+
+	cout << '\n';
+
+	for (int i = 0; i <= 10; i += 2) {
+		cout << i << '\n';
+	}
+	cout << "Happy New Year" << '\n';
+
+	cout << '\n';
+
+	for (int i = 0; i <= 10; i += 3) {
+		cout << i << '\n';
+	}
+	cout << "Happy New Year" << '\n';
+
+	cout << '\n';
+
+	for (int i = 10; i >= 0; i--) {
+		cout << i << '\n';
+	}
+
+	cout << "Happy New Year" << '\n';
+
+	cout << '\n';
+
+	for (int i = 10; i >= 0; i -= 2) {
+		cout << i << '\n';
+	}
+	cout << "Happy New Year" << '\n';
+
+}
